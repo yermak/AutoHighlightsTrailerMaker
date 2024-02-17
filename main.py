258e09cf9f30d8693ca5b0d8cba92e8ebcdb60c2
Yarick Yermak <yermak@gmail.com>
2024-02-17 08:50:40 +0000

    minor bug fixes

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ max_scene_distance = 60
 scene_difference = 0.15
 hardware = "-hwaccel nvdec -hwaccel_output_format cuda"
 # hardware = ""
-vc = "-c:v libx265 -crf 17 -preset superfast"
+vc = "-c:v libx265 -crf 0 -preset superfast"
 # vc = "-c:v libsvtav1 -crf 25 -preset 10 -svtav1-params fast-decode=1 -g 300"
 vc_final = "-c:v hevc_nvenc -profile:v main10 -pix_fmt p010le -rc:v vbr -tune hq -preset p5 -multipass 2 -bf 4 -b_ref_mode 1 -nonref_p 1 -rc-lookahead 75 -spatial-aq 1 -aq-strength 8 -temporal-aq 1 -cq 25 -qmin 23 -qmax 30 -b:v 1M -maxrate:v 3M"
 vf = """-vf "hwdownload,format=nv12" """
 # vf = ""
 # ac = "-c:a aac -b:a 192k -ac 2 -ar 44100"
 ac = "-c:a libopus -b:a 96k -ac 2"
+filter_complex = """-filter_complex "[0:a]asplit=2[sc][mix];[1:a][sc]sidechaincompress=threshold=0.006:ratio=19:attack=1000:release=3000:link=maximum:detection=peak[bg];[bg][mix]amerge[final]" """
 
 
 # accept array of strings and print them
@@ -68,15 +69,18 @@ def mk_tmp_dir(file):
 
 
 # read the time.txt file in tmp_dir and collect a list of timestamps for the trailer, time.txt has following format: 1st line: "frame:4    pts:8883875 pts_time:296.129", each 2nd line should be ignored
-def collect_pts_timestamps(tmp_dir, skip):
+def collect_pts_timestamps(time_file, skip):
     timestamps = []
-    with open(f'{tmp_dir}\\time.txt', 'r') as file:
+    with open(time_file, 'r') as file:
         lines = file.readlines()
-        for i in range(0, len(lines), 2):
+        for i in range(0, len(lines), 1):
             # extract timestamp from line and append it to timestamps list
-            ts = float(lines[i].split("pts_time:")[1])
-            if ts > skip:
-                timestamps.append(ts)
+            split = lines[i].split("pts_time:")
+            if (len(split) == 2):
+                pts_time = split[1]
+                ts = float(pts_time)
+                if ts > skip:
+                    timestamps.append(ts)
                 # pts.append(int(lines[i].split("pts:")[1].split(" ")[0]))
     return timestamps
 
@@ -95,7 +99,7 @@ def write_file(list_file, scenes):
 def make_trailer(video, music, skip):
     # run ffprobe to get the duration of the music file into the variable, reading directly from the output
     # ffprobe -i "input.mp3" -show_entries format=duration -v quiet -of csv="p=0"
-    music_duration = getMediaDuration(music)
+    music_duration = get_media_duration(music)
 
     # video_duration = getMediaDuration(video)
 
@@ -107,13 +111,13 @@ def make_trailer(video, music, skip):
     tmp_dir = mk_tmp_dir(video)
 
     # escape backslashes in the file path (for windows) and filters (for ffmpeg)
-    time_file = os.path.join(tmp_dir, 'time.txt').replace('\\', '\\\\').replace(':', '\\:')
-    print("Time file:", time_file)
-    # ffmpeg_keyframes = f"""ffmpeg.exe -i "{video}" -vf "select='eq(pict_type,I)" -vsync vfr -f null NULL"""
+    time_file = os.path.join(tmp_dir, 'time.txt')
+    time_file_fixed = time_file.replace('\\', '\\\\').replace(':', '\\:')
 
-    detectScenes(time_file, video)
+    print("Time file:", time_file_fixed)
+    detect_scenes(video, time_file_fixed)
 
-    timestamps = collect_pts_timestamps(tmp_dir, skip)
+    timestamps = collect_pts_timestamps(time_file, skip)
 
     # print(pts)
     print(timestamps)
@@ -122,7 +126,6 @@ def make_trailer(video, music, skip):
     random.seed()
 
     scenes = cut_by_timestamp(music_duration, timestamps, tmp_dir, video, skip)
-    # cut_by_frame(scenes, segment, pts, tmp_dir, video, video_fps)
 
     list_file = os.path.join(tmp_dir, 'list.txt')
     write_file(list_file, scenes)
@@ -130,15 +133,18 @@ def make_trailer(video, music, skip):
     print(f"Result file: {result_file}")
 
     # concatenate all scenes into one file and mux it with the music file
-    ffmpeg_concat = f"""ffmpeg.exe -f concat -safe 0 -i {list_file} -c:v copy -c:a copy "{result_file}" """
+    ffmpeg_concat = f"""ffmpeg.exe -f concat -safe 0 -i "{list_file}" -c:v copy -c:a copy "{result_file}" """
     print(ffmpeg_concat)
-    os.system(f"{ffmpeg_concat}")
+
+    if os.system(ffmpeg_concat) != 0:
+        print(f"Error in ffmpeg command, {ffmpeg_concat}")
+        exit(1)
 
     trailer_file = os.path.join(os.path.dirname(video), f"{os.path.basename(video)}_trailer.mkv")
     print(f"Result file: {trailer_file}")
 
     # concatenate all scenes into one file and mux it with the music file
-    ffmpeg_mux = f"""ffmpeg.exe -i  "{result_file}" -i "{music}" -filter_complex "[0:a]asplit=2[sc][mix];[1:a][sc]sidechaincompress=threshold=0.006:ratio=19:attack=1000:release=3000:link=maximum:detection=peak[bg];[bg][mix]amerge[final]" -map 0:v {vc_final} -map [final] {ac} -movflags +faststart -y "{trailer_file}" """
+    ffmpeg_mux = f"""ffmpeg.exe -i  "{result_file}" -i "{music}" {filter_complex} -map 0:v {vc_final} -map [final] {ac} -movflags +faststart -y "{trailer_file}" """
     print(ffmpeg_mux)
     os.system(f"{ffmpeg_mux}")
 
@@ -147,18 +153,23 @@ def make_trailer(video, music, skip):
     return 0
 
 
-def detectScenes(time_file, video):
+def detect_scenes(video, time_file):
     if skip != 0:
         skip_time = format_time(skip)
     ffmpeg_scenes = f"""ffmpeg.exe -i "{video}" -filter:v "select='isnan(prev_selected_t)+gte(t-prev_selected_t\\,{max_scene_distance})*eq(pict_type\\,PICT_TYPE_I)+gte(t-prev_selected_t\\,{scene_distance})*gt(scene,{scene_difference})*eq(pict_type\\,PICT_TYPE_I)',showinfo,metadata=print:file='{time_file}'" -fps_mode vfr -f null NULL"""
     print(ffmpeg_scenes)
     # measure time of execution
     start_time = time.time()
-    os.system(f"{ffmpeg_scenes}")
+
+    # run the command and check exit code if it is not 0, check std and err of the process and print it
+    if os.system(ffmpeg_scenes) != 0:
+        print(f"Error in ffmpeg command:\n {ffmpeg_scenes}")
+        exit(1)
+
     print(f"ffmpeg scenes took {time.time() - start_time} seconds")
 
 
-def getMediaDuration(media_file):
+def get_media_duration(media_file):
     ffprobe = f"""ffprobe.exe -i "{media_file}" -show_entries format=duration -v quiet -of csv="p=0" """
     print(ffprobe)
     # read media duration from the output to float
@@ -191,11 +202,13 @@ def cut_by_timestamp(music_duration, timestamps, tmp_dir, video, skip):
         duration = format_time(segment_duration)
         output_file = os.path.join(tmp_dir, f"scene_{i:04d}.ts")
         # ffmpeg_cut = f"""ffmpeg.exe {hardware} -ss {start} -i "{video}" -t {duration} {vf} -map 0:v {vc} -map 0:a  -c:a copy {output_file}"""
-        ffmpeg_cut = f"""ffmpeg.exe {hardware} -ss {start} -i "{video}" -t {duration} -map 0:v -map 0:a -c:v copy -avoid_negative_ts 1 -c:a copy {output_file}"""
+        ffmpeg_cut = f"""ffmpeg.exe {hardware} -ss {start} -i "{video}" -t {duration} -map 0:v -map 0:a -c:v copy -avoid_negative_ts 1 -c:a copy "{output_file}" """
         print(ffmpeg_cut)
-        os.system(f"{ffmpeg_cut}")
+        if os.system(ffmpeg_cut) != 0:
+            print(f"Error in ffmpeg command:\n {ffmpeg_cut}")
+            exit(1)
 
-        real_duration = getMediaDuration(output_file)
+        real_duration = get_media_duration(output_file)
         last_position = timestamps[i] + real_duration
         remaining_duration = remaining_duration - real_duration
         print(f"Real duration of {output_file}: {real_duration}")
